834e9cd57f29687d2b6f9a4cf38d86eb2b1b5d28
basilgoodluck <basilgoodluck22@gmail.com>
2024-12-08 09:03:44 +0100

    rocket chat deployed using docker

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <iframe
+      src='http://localhost:3000/channel/general'
+      width="100%"
+      height="100%"
+    />
+  )
+}
+
+export default App
